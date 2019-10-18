e0f28a843b516ef595ff524d0969f309837d6cfc
pjqdyd <1909025079@qq.com>
2019-10-18 11:03:16 +0800

    使用Ribbon实现负载均衡

--- spring-cloud-consumer-ribbon/src/main/java/com/pjqdyd/config/RibbonConfiguration.java
+++ spring-cloud-consumer-ribbon/src/main/java/com/pjqdyd/config/RibbonConfiguration.java
@@ -1,10 +1,7 @@
 package com.pjqdyd.config;
 
-import com.netflix.client.config.DefaultClientConfigImpl;
-import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
