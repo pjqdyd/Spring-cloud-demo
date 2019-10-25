99fe6f9eff4c7885116dfe69416112fc313e8556
pjqdyd <1909025079@qq.com>
2019-10-25 13:08:48 +0800

    update

--- spring-cloud-config-client/src/main/java/com/pjqdyd/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/pjqdyd/SpringCloudConfigClientApplication.java
@@ -2,13 +2,14 @@ package com.pjqdyd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**   
  * @Description:  [SpringCloud Config客户端]
  * @Author:       pjqdyd
  * @Version:      [v1.0.0]
  */
-
+//@EnableDiscoveryClient //引入并开启了eureka的话就要这个注解
 @SpringBootApplication
 public class SpringCloudConfigClientApplication {
 
