0dd24ed908d4c21a6385a11b05317b037af228b8
pjqdyd <1909025079@qq.com>
2019-10-25 11:28:40 +0800

    SpringCloud config

---
+++ spring-cloud-config-client/src/main/java/com/pjqdyd/SpringCloudConfigClientApplication.java
@@ -0,0 +1,19 @@
+package com.pjqdyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**   
+ * @Description:  [SpringCloud Config客户端]
+ * @Author:       pjqdyd
+ * @Version:      [v1.0.0]
+ */
+
+@SpringBootApplication
+public class SpringCloudConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+    }
+
+}
