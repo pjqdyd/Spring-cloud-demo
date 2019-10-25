12783cda4f18c2fe109e8b07c1e53cc892ccd379
pjqdyd <1909025079@qq.com>
2019-10-25 13:22:54 +0800

    update

--- spring-cloud-config-client/src/main/java/com/pjqdyd/controller/ConfigClientController.java
+++ spring-cloud-config-client/src/main/java/com/pjqdyd/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.pjqdyd.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //开启刷新配置(默认不刷新,需要手动刷新)
 public class ConfigClientController {
 
     @Value("${profile}")
